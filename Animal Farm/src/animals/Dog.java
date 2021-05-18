210c575ed31ce58f5a1095764b5653baa9f0871c
sophr329 <84166707+sophr329@users.noreply.github.com>
2021-05-18 08:02:08 -0500

    Get/Set
    Created getters and setters for all four instance variables

--- Animal Farm/src/animals/Dog.java
+++ Animal Farm/src/animals/Dog.java
@@ -7,4 +7,61 @@ public class Dog {
 	private String color;
 	private int age;
 	
+	
+	
+	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the breed
+	 */
+	public String getBreed() {
+		return breed;
+	}
+	/**
+	 * @param breed the breed to set
+	 */
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
